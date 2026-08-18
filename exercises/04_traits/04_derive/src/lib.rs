094a4622d9ac30bc04200900df51717416d85e6c
reyhanquayum <reyhan.quayum@gmail.com>
2026-08-18 04:05:30 -0400

    types, traits, overloads, extending functionality of types, generics

--- exercises/04_traits/04_derive/src/lib.rs
+++ exercises/04_traits/04_derive/src/lib.rs
@@ -8,12 +8,19 @@
 // print both sides of the comparison to the terminal.
 // If the compared type doesn't implement `Debug`, it doesn't know how to represent them!
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 struct Ticket {
     title: String,
     description: String,
     status: String,
 }
+
+// impl std::fmt::Debug for Ticket {
+//     fn fmt(&self, _: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+//         todo!()
+//         println!()
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
